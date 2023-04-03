26e80ee0ecc5e0d31daf05c45ae05652bde93193
abbos0959 <abbosakromovich777@gmail.com>
2023-04-03 23:40:16 +0500

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from './config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(config.db as TypeOrmModuleOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+  db: {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'abbos1999',
+    database: 'NestMusic',
+    synchronize: true,
+  },
+};
